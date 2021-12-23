645256827522e6626bd4b52965e86da82dc69d53
Jessie Puls <jessiepuls@gmail.com>
2021-12-23 00:25:02 -0600

    Adding install script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dotfiles
+
+Managed with [Chezmoi](https://github.com/twpayne/chezmoi)
+
+
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+echo ""
+echo "🤚  This script will setup .dotfiles for you."
+read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
+
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsLS https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
+
+echo "chezmoi=$chezmoi"
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+echo "script_dir=$script_dir"
+
+# exec: replace current process with chezmoi init
+exec "$chezmoi" init --apply "--source=$script_dir"
